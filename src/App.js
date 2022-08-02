31306f5ae7511603b0fe3e72cbd57e374c8a8cb5
Guilherme <guilhermealarcao@gmail.com>
2022-08-02 17:10:04 -0300

    aula 4.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
+
   const [colaboradores, setColaboradores] = useState([]);
+
+
 
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -18,6 +32,7 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoSubmit={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      {times.map(item=><Time nome={item} />)}
     </div>
   );
 }
